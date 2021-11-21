82caa1027ec18590eb56c1c6aeb60c65086a3fa8
olinanderson <olin.anderson@ucalgary.ca>
2021-11-21 16:42:56 -0700

    production build

--- server.js
+++ server.js
@@ -67,12 +67,15 @@ app.get("/api/audio", async (req, res) => {
   return res.json(fileArray);
 });
 
-app.use(express.static(path.join(__dirname, "build")));
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 // Firing up the server on selected port
 server.listen(PORT);
 
